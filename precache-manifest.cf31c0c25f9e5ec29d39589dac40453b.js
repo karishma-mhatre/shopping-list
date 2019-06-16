30cd30106e23fc669b8e7345b6c80fe468026299
Apple <apple@192.168.1.3>
2019-06-16 23:02:36 +0530

    Updates

---
+++ precache-manifest.cf31c0c25f9e5ec29d39589dac40453b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8af5cadba81822eebc6fe82800e71d",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "74cf0c468279a4699d85",
+    "url": "/shopping-list/static/css/main.23dae9a4.chunk.css"
+  },
+  {
+    "revision": "2322de1ff88d3fd5d50d",
+    "url": "/shopping-list/static/js/2.6e4f2013.chunk.js"
+  },
+  {
+    "revision": "74cf0c468279a4699d85",
+    "url": "/shopping-list/static/js/main.7d0ab381.chunk.js"
+  },
+  {
+    "revision": "f83e989e4d16bb609454",
+    "url": "/shopping-list/static/js/runtime~main.e244f075.js"
+  },
+  {
+    "revision": "fc98297ae1cf2cdd36d17d4b5340602b",
+    "url": "/shopping-list/static/media/catban-420190517165547.fc98297a.webp"
+  },
+  {
+    "revision": "8dcc6cc2e2c0616086c4acdafe5d61fd",
+    "url": "/shopping-list/static/media/newlogo.8dcc6cc.8dcc6cc2.png"
+  }
+]);
